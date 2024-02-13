c3f0663cdf6cf443506ea6f00f075df6fa8b113f
stryn26 <sutrionoarif@gmail.com>
2024-02-13 18:11:52 +0700

    update resource

---
+++ app/Models/PmbModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PmbModel extends Model
+{
+    use HasFactory;
+}
