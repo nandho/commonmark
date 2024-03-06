cfe10ebe1d2bf7f9a096ceac12b20d089abba92c
stryn26 <sutrionoarif@gmail.com>
2024-03-06 21:59:57 +0700

    menambahkan fitur email

---
+++ resources/views/mail/test.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <style>
+        p {
+            font-size: 12px;
+        }
+
+        .signature {
+            font-style: italic;
+        }
+    </style>
+</head>
+
+<body>
+    <div>
+        <p>Halo {{$name}},<br></p>
+
+        <p>Kami dengan senang hati mengucapkan selamat datang dan selamat bergabung sebagai calon mahasiswa baru di <b>Akademi Pariwisata Dharma Nusantara Sakti (Akparda) Yogyakarta!</b> <br></p>
+
+        <p>Kami ingin mengucapkan terima kasih atas minat dan dedikasi Anda untuk menjadi bagian dari komunitas akademik kami. Kami percaya bahwa perjalanan pendidikan Anda di sini akan menjadi pengalaman yang memuaskan dan membawa banyak manfaat bagi Anda secara pribadi dan profesional. <br></p>
+
+        <p>Sebagai calon mahasiswa, Anda telah memilih untuk mengejar pengetahuan dan pengembangan diri, dan kami sangat bersemangat untuk mendukung Anda dalam perjalanan pendidikan Anda. Kami memiliki tim yang berdedikasi untuk membantu Anda menjelajahi minat akademik Anda, memberikan saran, dan mendukung Anda dalam mencapai tujuan Anda. <br></p>
+
+        <p>Berikut adalah beberapa langkah awal yang perlu Anda ambil untuk memulai perjalanan pendidikan Anda dengan kami:<br></p>
+
+        <p>Lakukan pendaftaran dan lengkapi semua dokumen yang diperlukan sebelum tanggal [Tentukan Batas Waktu]. <br></p>
+        <p>Jika Anda memiliki pertanyaan atau butuh bantuan, jangan ragu untuk menghubungi kami melalui [informasi kontak]. <br></p>
+        <p>Jangan lewatkan orientasi mahasiswa baru yang akan diadakan pada tanggal [Tentukan Tanggal Orientasi]. <br></p>
+        <p>Kami berharap bahwa pengalaman Anda di sini akan menjadi berkesan dan membangun. Kami yakin bahwa Anda akan menemukan banyak peluang untuk tumbuh, belajar, dan berkembang.<br></p>
+
+
+        <p>Sekali lagi, selamat bergabung dan semoga sukses dalam perjalanan akademik Anda di Akademi Pariwisata Dharma Nusantara Sakti (Akparda) Yogyakarta! <br></p>
+
+        <p>Salam hangat,</p>
+
+
+        <p class="signature">Admisi AKPARDA</p>
+        <p class="signature">Akademi Pariwisata Dharma Nusantara Sakti (Akparda) Yogyakarta!</p>
+    </div>
+</body>
+
+</html>
