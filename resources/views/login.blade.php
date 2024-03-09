63828eb0a14d7bda50f9fdc362f058444d48e403
stryn26 <sutrionoarif@gmail.com>
2024-03-09 10:38:24 +0000

    update fe

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -65,7 +65,7 @@
 
 
             // Kirim data menggunakan Axios
-            axios.post('http://202.91.8.170:9000/api/auth/login', {
+            axios.post('http://202.91.8.170:8000/api/auth/login', {
                     username: username,
                     password: pass
                 })
@@ -89,7 +89,7 @@
 
                     //redirect to page
                     //redirect to page
-                    // window.location.href = "/tes";
+                    window.location.href = "/tes";
                 })
                 .catch(function(error) {
                     console.error(error); // Log kesalahan jika terjadi
