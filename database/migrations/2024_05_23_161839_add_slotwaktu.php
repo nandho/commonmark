9375c6592db3e28e8ab0fe2a56a4a35f409f195b
Triyan31 <triyan31@stiesbi.ac.id>
2024-06-02 11:20:18 +0700

    update slopt waktu

--- database/migrations/2024_05_23_161839_add_slotwaktu.php
+++ database/migrations/2024_05_23_161839_add_slotwaktu.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('slot_waktu', function (Blueprint $table){
-            $table->uuid('uuid');
+            $table->uuid('id');
             $table->time('waktu');
         });
     }
