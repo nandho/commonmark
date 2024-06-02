6707b0ce1fc9175bad11f2cfd78afc7f37ea3638
Triyan31 <triyan31@stiesbi.ac.id>
2024-06-02 11:07:53 +0700

    update FE menu akun dosen

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -234,6 +234,87 @@
                 </li>
             </ul>
         </div>
+        <!-- Dosen -->
+        <div id="dosenItems" style="display: none;" class="items-center block w-auto max-h-screen overflow-auto grow basis-full">
+            <ul class="flex flex-col pl-0 mb-0">
+                <li class="mt-0.5 w-full">
+                    <a class="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="./pages/dashboard.html">
+                        <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                            <i class="relative top-0 text-sm leading-normal text-blue-500 ni ni-tv-2"></i>
+                        </div>
+                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="mt-0.5 w-full">
+                    <a class=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="/semester">
+                        <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                            <i class="relative top-0 text-sm leading-normal text-orange-500 ni ni-calendar-grid-58"></i>
+                        </div>
+                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">dumy</span>
+                    </a>
+                </li>
+
+                <li class="mt-0.5 w-full">
+                    <a class=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="./pages/billing.html">
+                        <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center fill-current stroke-0 text-center xl:p-2.5">
+                            <i class="relative top-0 text-sm leading-normal text-emerald-500 ni ni-credit-card"></i>
+                        </div>
+                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Billing</span>
+                    </a>
+                </li>
+
+                <li class="mt-0.5 w-full">
+                    <a class=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="./pages/virtual-reality.html">
+                        <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                            <i class="relative top-0 text-sm leading-normal text-cyan-500 ni ni-app"></i>
+                        </div>
+                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Virtual Reality</span>
+                    </a>
+                </li>
+
+                <li class="mt-0.5 w-full">
+                    <a class=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="./pages/rtl.html">
+                        <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                            <i class="relative top-0 text-sm leading-normal text-red-600 ni ni-world-2"></i>
+                        </div>
+                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">RTL</span>
+                    </a>
+                </li>
+
+                <li class="w-full mt-4">
+                    <h6 class="pl-6 ml-2 text-xs font-bold leading-tight uppercase dark:text-white opacity-60">Account
+                        pages</h6>
+                </li>
+
+                <li class="mt-0.5 w-full">
+                    <a id="detailsbiodata" class="dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors">
+                        <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                            <i class="relative top-0 text-sm leading-normal text-slate-700 ni ni-single-02"></i>
+                        </div>
+                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Biodata</span>
+                    </a>
+
+                </li>
+                <li class="mt-0.5 w-full">
+                    <a class=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="./pages/sign-in.html">
+                        <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                            <i class="relative top-0 text-sm leading-normal text-orange-500 ni ni-single-copy-04"></i>
+                        </div>
+                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Sign In</span>
+                    </a>
+                </li>
+
+                <li class="mt-0.5 w-full">
+                    <a class=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="./pages/sign-up.html">
+                        <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                            <i class="relative top-0 text-sm leading-normal text-cyan-500 ni ni-collection"></i>
+                        </div>
+                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Sign Up</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
         <!-- / End Navbar Menu -->
     </aside>
     <div class="relative h-full max-h-screen transition-all duration-200 ease-in-out xl:ml-68">
@@ -334,7 +415,6 @@
             }
 
             const role = data.role;
-
             // Tampilkan data di console log
             console.log("Role : " + role);
 
@@ -347,16 +427,67 @@
                 document.getElementById('pmbItems').style.display = 'block';
                 document.getElementById('calonMahasiswaItems').style.display = 'none';
                 document.getElementById('akademikItems').style.display = 'none';
+                document.getElementById('dosenItems').style.display = 'none';
             } else if (role[0] === 'calonmahasiswa') { // Ambil role dari array dengan indeks 0
                 console.log("mahasiswa");
                 document.getElementById('calonMahasiswaItems').style.display = 'block';
                 document.getElementById('pmbItems').style.display = 'none';
                 document.getElementById('akademikItems').style.display = 'none';
+                document.getElementById('dosenItems').style.display = 'none';
             } else if (role[0] === 'akademik') { // Ambil role dari array dengan indeks 0
                 console.log("akademik");
                 document.getElementById('calonMahasiswaItems').style.display = 'none';
                 document.getElementById('pmbItems').style.display = 'none';
                 document.getElementById('akademikItems').style.display = 'block';
+                document.getElementById('dosenItems').style.display = 'none';
+            } else if (role[0] === 'dosen') { // Ambil role dari array dengan indeks 0
+                console.log("Dosen");
+                document.getElementById('calonMahasiswaItems').style.display = 'none';
+                document.getElementById('pmbItems').style.display = 'none';
+                document.getElementById('akademikItems').style.display = 'none';
+                document.getElementById('dosenItems').style.display = 'block';
+                document.addEventListener('DOMContentLoaded', function() {
+                    const token = getCookie('token'); // Ambil token dari cookie
+                    if (!token) {
+                        console.error('Token is not defined');
+                        return; // Token tidak ditemukan, hentikan eksekusi script
+                    }
+
+                    const axiosInstance = axios.create({
+                        baseURL: 'http://localhost:9000/api',
+                        headers: {
+                            'Authorization': `Bearer ${token}` // Sertakan token yang aman
+                        }
+                    });
+
+                    axiosInstance.get(`/user/${id}`)
+                        .then(response => {
+                            const user = response.data.data;
+                            // Dapatkan id_akun dari data dosen
+                            const userid = user.id;
+                            const dosenId = user.id;
+                            // Tetapkan href
+                            document.getElementById('detailsbiodata').addEventListener('click', function() {
+                                window.location.href = `/dosen/${dosenId}`;
+                            });
+                        })
+                        .catch(error => {
+                            console.error('Error fetching data:', error);
+                        });
+
+                    // Fungsi untuk membaca cookie
+                    function getCookie(name) {
+                        let cookieArr = document.cookie.split(";");
+                        for (let i = 0; i < cookieArr.length; i++) {
+                            let cookiePair = cookieArr[i].split("=");
+                            if (name == cookiePair[0].trim()) {
+                                return decodeURIComponent(cookiePair[1]);
+                            }
+                        }
+                        return null;
+                    }
+                });
+
             } else {
                 console.log("Role tidak dikenali");
             }
