ab3b0fd0a0c8d1ddc65724dbe307f25a07d79845
stryn26 <sutrionoarif@gmail.com>
2024-06-08 13:28:50 +0700

    update comment siakad

--- database/migrations/2024_02_13_120139_create_krs_models_table.php
+++ database/migrations/2024_02_13_120139_create_krs_models_table.php
@@ -9,23 +9,23 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('krs', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->string('periode_mulai');
-            $table->string('periode_berakhir');
-            $table->string('semester');
-            $table->string('status');
-            $table->timestamps();
-        });
-    }
+    // public function up(): void
+    // {
+    //     Schema::create('krs', function (Blueprint $table) {
+    //         $table->uuid('id')->primary();
+    //         $table->string('periode_mulai');
+    //         $table->string('periode_berakhir');
+    //         $table->string('semester');
+    //         $table->string('status');
+    //         $table->timestamps();
+    //     });
+    // }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('krs_models');
-    }
+    // /**
+    //  * Reverse the migrations.
+    //  */
+    // public function down(): void
+    // {
+    //     Schema::dropIfExists('krs_models');
+    // }
 };
