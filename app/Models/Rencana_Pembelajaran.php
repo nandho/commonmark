e844d9a0ef90aec2797b49c6f433c431336ed6c2
MieAyam- <z9nandho@gmail.com>
2024-06-09 22:35:38 +0700

    rencana eval

---
+++ app/Models/Rencana_Pembelajaran.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rencana_Pembelajaran extends Model
+{
+    use HasFactory;
+}
